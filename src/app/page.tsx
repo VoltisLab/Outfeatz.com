467ca7bf1965ccf99319c4ae9f406abfa348add2
Abdullahi Olawale Mukaila <ayomikail21@gmail.com>
2025-03-25 11:58:07 +0100

    page.tsx
    For Mobile Responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,17 @@ import Showcase from "@/components/sections/showcase";
 import Playground from "@/components/sections/playground";
 import Footer from "@/components/sections/footer";
 
-
-
-
 export default function Home() {
   return (
-    <div className="page-container bg-black w-full min-h-screen">       
-    <Hero />
-    <Featured/>
-    <Showcase/>
-    <FashionSection/>
-    <Playground/>
-    <Footer/>
+    <div className="bg-black w-full min-h-screen">
+      <div className="max-w-[var(--max-width)] mx-auto px-4 sm:px-6 lg:px-8">
+        <Hero />
+        <Featured />
+        <Showcase />
+        <FashionSection />
+        <Playground />
+      </div>
+      <Footer />
     </div>
   );
 }
